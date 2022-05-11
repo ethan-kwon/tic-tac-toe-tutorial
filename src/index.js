d6ca4ba161f4260195b5e9615f3740ca27b7c576
Ethan Kwon <ethan200304@gmail.com>
2022-05-10 20:24:26 -0400

    Finished improvement 3

--- src/index.js
+++ src/index.js
@@ -20,25 +20,31 @@ class Board extends React.Component {
     );
   }
 
+  renderRow(row) {
+    const squares = [];
+    for (let s = 0; s < 3; s++) {
+      squares.push(
+        this.renderSquare(row * 3 + s)
+      );
+    }
+    return (
+      <div className="board-row">
+        {squares}
+      </div>
+    )
+  }
+
   render() {
-    
+    const board = [];
+    for (let i = 0; i < 3; i++) {
+      board.push(
+        this.renderRow(i)
+      );
+    }
+
     return (
       <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+        {board}
       </div>
     );
   }
